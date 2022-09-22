110d860856061c47206db48b13be99b82fcf0f49
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-09-21 21:41:39 -1000

    convert from callback to promise

--- getWeather.js
+++ getWeather.js
@@ -1,10 +1,13 @@
-function getWeather(apiKey, zip, callback) {
+async function getWeather(apiKey, zip) {
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    fetch(path)
-        .then(res => res.json())
-        .then(json => callback(json))
-        .catch(err => console.log(err.message))
+    try {
+        const res = await fetch(path)
+        const json = await res.json()
+        return json 
+    } catch(error) {
+        console.log(error.message)
+    }
 }
 
 export { getWeather }
