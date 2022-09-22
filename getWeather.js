b00ac831e35cb8559623216b4e1d180e14a48182
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-09-21 21:06:32 -1000

    challenge 1

---
+++ getWeather.js
@@ -0,0 +1,14 @@
+function getWeather(apiKey, zip) {
+    // const apiKey = '467355df4c808dd6134a3b64e9ace282'
+    const units = 'imperial'
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+    fetch(path)
+        .then(res => res.json())
+        .then(json => {
+        tempEl.innerHTML = json.main.temp
+        descEl.innerHTML = json.weather[0].description
+        })
+        .catch(err => console.log(err.message))
+}
+
+export { getWeather }
