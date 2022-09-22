d1166ab763da745bda76dec11fb5d7735f055099
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-09-21 22:12:25 -1000

    challenge 7, add methods to get weather by city and geolocation

--- getWeather.js
+++ getWeather.js
@@ -1,5 +1,4 @@
-async function getWeather(apiKey, zip, units = 'imperial') {
-    console.log (units)
+async function getWeatherByZip(apiKey, zip, units = 'imperial') {
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     try {
         const res = await fetch(path)
@@ -16,4 +15,38 @@ async function getWeather(apiKey, zip, units = 'imperial') {
     }
 }
 
-export { getWeather }
+async function getWeatherByCity(apiKey, city, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?city=${city}&appid=${apiKey}&units=${units}`
+    try {
+        const res = await fetch(path)
+        const json = await res.json()
+        console.log(json)
+        return {
+            name: json.name,
+            temp: json.main.temp,
+            feels_like: json.main.feels_like,
+            weather: json.weather[0].description
+        } 
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+
+async function getWeatherByGeo(apiKey, lat, lon, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+    try {
+        const res = await fetch(path)
+        const json = await res.json()
+        console.log(json)
+        return {
+            name: json.name,
+            temp: json.main.temp,
+            feels_like: json.main.feels_like,
+            weather: json.weather[0].description
+        } 
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+
+export { getWeatherByZip, getWeatherByCity, getWeatherByGeo }
